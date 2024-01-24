333c1e6eeb013b600f3ffffc8fac78faacb27c3f
TALHADWA <151826020+TALHADWA@users.noreply.github.com>
2024-01-24 10:05:30 -0800

    Update model.js

--- model.js
+++ model.js
@@ -1,1 +1,22 @@
 
+const monogos=require("mongoose");
+const dta=monogos.Schema({
+    images:{
+ type:Array,
+    },
+    whocontains:{
+        type:monogos.Schema.Types.ObjectId, ref:"Userss" ,  required:true
+    },
+    title: { type: String, default: 'hahaha' },
+     subtitle: { type:String, },
+    // bio: { type: String, match: /[a-z]/ },
+    // date: { type: Date, default: Date.now },
+    // buff: Buffer
+});
+
+// dta.pre("save", function(next){
+//    this.name="Fuck"
+//     console.log(this.name);
+
+// });
+module.exports=monogos.model("Fuck",dta);
